81ebcabbd8055f8cf5eaca75ebe8ce695e75402f
NgQHung <hunghunghung273@gmail.com>
2022-08-13 10:50:49 +0200

    added require auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { loginUser, signupUser } = require("../controllers/userController");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
 
+// require authentication for all routes
+// router.use(requireAuth);
+
 // login
 router.post("/login", loginUser);
-
-// get
-router.get("/loginn", loginUser);
 
 // signup
 router.post("/signup", signupUser);
