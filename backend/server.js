5b4479bfa894db18542c38ec558306131f8c38f0
NgQHung <hunghunghung273@gmail.com>
2023-09-14 22:33:03 +0200

    hotfixes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const routes = require("./routes/routes");
-const userRoutes = require("./routes/userRoutes");
-require("dotenv").config();
-const mongoose = require("mongoose");
+const express = require('express');
+const routes = require('./routes/routes');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
@@ -11,18 +11,19 @@ const app = express();
 app.use(express.json());
 
 // routes
-app.use("/api/tavern/", routes);
-app.use("/api/user", userRoutes);
+app.use('/api/tavern/', routes);
+app.use('/api/user', userRoutes);
 app.use((req, res, next) => {
     next();
 });
 
 // connect to db
+mongoose.set('strictQuery', false);
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log("connected to mongo DB && listening on the port", process.env.PORT);
+            console.log('connected to mongo DB && listening on the port', process.env.PORT);
         });
     })
     .catch((err) => {
